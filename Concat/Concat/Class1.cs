d6ba51f28d475e5655173f30aadb2d1b2d383fc7
ruslan <ruslan_aliullin@epam.com>
2024-04-04 22:58:04 +0200

    Revert "new"
    This reverts commit 36c5bf8acbe6a605dba41a340a0591424ad10550.

---
+++ Concat/Concat/Class1.cs
@@ -0,0 +1,9 @@
+﻿public class MyClass
+{
+    public string Concat(string text)
+    {
+        DateTime currentTime = DateTime.Now;
+        string date = currentTime.ToString("HH:mm:ss");
+            return date+" Hello, "+text;
+    }
+}
