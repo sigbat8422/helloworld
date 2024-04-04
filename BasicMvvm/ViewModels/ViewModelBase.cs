07c88992978091bd271536554b38fa1f3d940995
ruslan <ruslan_aliullin@epam.com>
2024-04-04 23:07:36 +0200

    new2

--- BasicMvvm/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using ReactiveUI;
-
-namespace BasicMvvmSample.ViewModels;
-
-public class ViewModelBase : ReactiveObject
-{
-}
--- Concat/Concat/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿public class MyClass
-{
-    public string Concat(string text)
-    {
-        DateTime currentTime = DateTime.Now;
-        string date = currentTime.ToString("HH:mm:ss");
-            return date+" Hello, "+text;
-    }
-}
