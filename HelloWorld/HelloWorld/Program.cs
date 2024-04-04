e54bdf440cfbd5bbcb86d9dfc266bc4c7203d3ae
ruslan <ruslan_aliullin@epam.com>
2024-04-04 22:38:32 +0200

    new2

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please enter your name");
+var name = Console.ReadLine();
+var myClass = new MyClass();
+string result = myClass.Concat(name);
+Console.WriteLine(result);
